f2c9f91b630f65101c4642ac67a8686282707245
Ashkan Zarifian <zs.ashkan@yahoo.com>
2022-11-09 12:31:49 +0300

    Add browser localstorage support

--- src/store/TodoContext.tsx
+++ src/store/TodoContext.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useState } from "react";
+import React, { PropsWithChildren, useEffect, useState } from "react";
 import Todo from "../model/Todo";
 
 type TodoContextType = {
@@ -20,14 +20,27 @@ export const TodoContext = React.createContext<TodoContextType>({
 const TodoContextProvider: React.FC<PropsWithChildren> = (props) => {
   const [todoItems, setTodoItems] = useState<Todo[]>([]);
 
+  useEffect(() => {
+    const savedItems = localStorage.getItem("todoLists");
+    if (savedItems !== null) {
+      setTodoItems(JSON.parse(savedItems));
+    }
+  }, []);
+
   const addTodoHandler = (todoText: string) => {
     const newTodo = new Todo(todoText);
     setTodoItems((prevState) => prevState.concat(newTodo));
+    localStorage.setItem("todoLists", JSON.stringify(todoItems));
   };
 
   const removeTodoHandler = (id: string) => {
     setTodoItems((prevState) => {
       const newTodoItems = prevState.filter((item) => item.id !== id);
+      if (newTodoItems.length > 0) {
+        localStorage.setItem("todoLists", JSON.stringify(newTodoItems));
+      } else {
+        localStorage.removeItem("todoLists");
+      }
       return newTodoItems;
     });
   };
@@ -37,6 +50,7 @@ const TodoContextProvider: React.FC<PropsWithChildren> = (props) => {
       const indexOfItem = prevState.findIndex((item) => item.id === id);
       const newItems = prevState;
       newItems[indexOfItem].isCompleted = isChecked;
+      localStorage.setItem("todoLists", JSON.stringify(newItems));
       return newItems;
     });
   };
@@ -44,6 +58,11 @@ const TodoContextProvider: React.FC<PropsWithChildren> = (props) => {
   const clearCompletedHandler = () => {
     setTodoItems((prevState) => {
       const newTodoList = prevState.filter((item) => !item.isCompleted);
+      if (newTodoList.length > 0) {
+        localStorage.setItem("todoLists", JSON.stringify(newTodoList));
+      } else {
+        localStorage.removeItem("todoLists");
+      }
       return newTodoList;
     });
   };
